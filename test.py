f8e012b10840ab3d64276eb751d671a918947f21
Pete Bachant <petebachant@gmail.com>
2015-04-02 10:51:36 -0400

    Accept style command line arg to test.py

--- test.py
+++ test.py
@@ -2,17 +2,27 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-plt.style.use("./arial.mplstyle")
 
-data = np.linspace(0, 1)
+def plot(style):
+    plt.style.use("./{}.mplstyle".format(style))
+    data = np.linspace(0, 1)
+    plt.figure()
+    for n in range(8):
+        plt.plot(data + 0.2*n, label="Line {}".format(n))
+    plt.xlabel("$x$-label")
+    plt.ylabel("$y$-label")
+    plt.title("Title $f = ma$")
+    plt.grid(True)
+    plt.legend(loc="best")
+    plt.show()
 
-for n in range(8):
-    plt.plot(data + 0.2*n, label="Line {}".format(n))
 
-plt.xlabel("$x$-label")
-plt.ylabel("$y$-label")
-plt.title("Title $f = ma$")
-plt.grid(True)
-plt.legend(loc="best")
-plt.show()
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        style = sys.argv[1]
+    else:
+        style = "arial"
+    
+    plot(style)
