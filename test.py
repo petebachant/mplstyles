bf476bf3b4ab9bb9cac2046ff4b82fa5c2f3c909
Pete Bachant <petebachant@gmail.com>
2015-04-02 10:38:14 -0400

    Remove blank line

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ data = np.linspace(0, 1)
 for n in range(8):
     plt.plot(data + 0.2*n, label="Line {}".format(n))
 
-
 plt.xlabel("$x$-label")
 plt.ylabel("$y$-label")
 plt.title("Title $f = ma$")
